c6f3710abf8400dd012b60d987c8aac40bb4cf4f
Matthew Zhang <mtz99@users.noreply.github.com>
2025-03-07 00:18:35 -0800

    Starting a basic template for the calculator

--- just-a-plane-calculator/MainWindow.xaml.cs
+++ just-a-plane-calculator/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using just_a_plane_calculator;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +23,119 @@ namespace just_a_plane_calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string display = "0";
+        private string history = "";
+        private bool ClearScreen = true;
+        private bool decimalInserted = false;
+
         public MainWindow()
         {
             InitializeComponent();
+            Update();
+        }
+
+        private void InputNumber(object sender, RoutedEventArgs e)
+        {
+            string num = (sender as Button).Name.ToString().Remove(0, 1);
+
+            if (display[0] == '0' && num == "0") { return; }
+            if (ClearScreen) { display = ""; }
+
+            display += num;
+            ClearScreen = false;
+            Update();
+        }
+
+        private void InputDecimal(object sender, RoutedEventArgs e)
+        {
+            if (ClearScreen) { display = "0"; }
+            if (decimalInserted) { return; }
+            display += (sender as Button).Content;
+            decimalInserted = true;
+            ClearScreen = false;
+            Update();
+        }
+
+        private void InputArithmatic(object sender, RoutedEventArgs e)
+        {
+            if (display == "") { return; }
+            ClearScreen = true;
+            decimalInserted = false;
+            history = display + $" {(sender as Button).Content} ";
+            Update();
+        }
+
+        private void EvaluateArithmatic(object sender, RoutedEventArgs e)
+        {
+            if (history == "") { return; }
+            // a really scuffed way to allow for the repeated input of an operation
+            // basically if yuou press '=' after putting in something like 3 + 3
+            // it will ad 3 to the value of the operation (so history will say 6 + 3)
+
+            string inputArithmatic;
+
+            if (history.Contains("="))
+            {
+                string[] temp = history.Split(' '); // 3 + 20 =  ->  '3', '+', '20', '='
+                temp[0] = display;
+                inputArithmatic = String.Join(" ", temp);
+
+                history = inputArithmatic;
+            }
+            else
+            {
+                inputArithmatic = history + display;
+                history = inputArithmatic + " = ";
+            }
+
+            display = Calculate.CalculateString(inputArithmatic);
+            ClearScreen = true;
+            decimalInserted = false;
+            Update();
+        }
+
+        private void FlipNum(object sender, RoutedEventArgs e)
+        {
+            if (display == "0") { return; }
+            float tempNum = -1 * float.Parse(display);
+            display = tempNum.ToString();
+            Update();
+        }
+
+        private void InputClear(object sender, RoutedEventArgs e)
+        {
+            display = "0";
+            history = "";
+            ClearScreen = true;
+            Update();
+        }
+
+        private void TrimNumberInput(object sender, RoutedEventArgs e)
+        {
+            if (display == "0") { return; }
+            if (display.Length == 1 || display.Length == 2 && display.Contains("-"))
+            {
+                display = "0";
+                ClearScreen = true;
+            }
+            else
+            {
+                display = display.Remove(display.Length - 1, 1);
+            }
+            Update();
+        }
+
+        private void SqaureNum(object sender, RoutedEventArgs e)
+        {
+            if (display == "0") { return; }
+            display = Calculate.Square(display);
+            Update();
+        }
+
+        private void Update()
+        {
+            InputScreen.Text = display;
+            InputHistory.Text = history;
         }
     }
 }
---
+++ just-a-plane-calculator/calculate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace just_a_plane_calculator
+{
+    public class Calculate
+    {
+        public static string CalculateString(string input)
+        {
+            if (input.Length == 0) 
+            { 
+                return ""; 
+            }
+            
+            string[] splitInput = input.Split(' ');
+
+            float output = 0;
+            float leftInput = float.Parse(splitInput[0]);
+            float rightInput = float.Parse(splitInput[2]);
+
+            switch (splitInput[1])
+            {
+                case "+":
+                    output = leftInput + rightInput;
+                    break;
+                case "-":
+                    output = leftInput - rightInput;
+                    break;
+                case "*":
+                    output = leftInput * rightInput;
+                    break;
+                case "/":
+                    output = leftInput / rightInput;
+                    break;
+            }
+
+            return output.ToString();
+        }
+
+        public static string Square(string input)
+        {
+            float temp = float.Parse(input);
+            return (temp * temp).ToString();
+        }
+    }
+}
